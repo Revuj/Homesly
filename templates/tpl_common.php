9badbf34a705ab6e713cebfdcab04930cfd4723c
João Rafael Varela <revuj112@gmail.com>
2019-11-13 18:02:23 +0000

    Search form on main page

--- templates/tpl_common.php
+++ templates/tpl_common.php
@@ -9,6 +9,7 @@ function draw_header() { ?>
         <title>HOME.LY</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link rel="stylesheet" type="text/css" href="../css/mainpagestyle.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="../js/main.js" defer></script>
     </head>
--- templates/tpl_main.php
+++ templates/tpl_main.php
@@ -1,7 +1,16 @@
 <?php
 function draw_main_page() { ?>
     <div class="bgimg-1">
-
+        <section id="search-1">
+            <p>Travel Anywhere...</p>
+            <form method="post" action="../actions/action_search.php">
+                <label>Location<input type="text" name="location" placeholder="Where" ></label>
+                <label>Checkin<input type="date" name="checkin" placeholder="mm/dd/yyyy" ></label>
+                <label>Checkout<input type="date" name="checkout" placeholder="mm/dd/yyyy" ></label>
+                <label>Guests<input type="number" min="0" name="guests" placeholder="Guests" ></label>
+                <input type="submit" value="Search">
+            </form>
+        </section>
     </div>
 <?php }
 ?>
