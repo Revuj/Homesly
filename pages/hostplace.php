8a6398cf5454dae81b1972d9a93002052487f43f
João Rafael Varela <revuj112@gmail.com>
2019-11-25 13:08:27 +0000

    Added item.php

--- pages/hostplace.php
+++ pages/hostplace.php
@@ -2,6 +2,6 @@
     include_once('../templates/tpl_common.php');
     include_once('../templates/tpl_places.php');
     draw_header();
-    draw_input_place(); //to implement in tpl_common (check mockups)
+    draw_input_place(); 
     draw_footer();
 ?>
--- templates/tpl_places.php
+++ templates/tpl_places.php
@@ -18,10 +18,12 @@ function listPlaces($places) { ?>
  */
 function listItem($place) { ?>
   <article class="place_overview">
-    <img src="../images/test.jpg" />
-    <p> <?=$place['place_price_per_day']?> </p>
-    <p> <?=$place['place_title']?> </p>
-    <p> <?=$place['place_location']?> </p>
+    <a href="../pages/item.php?id=<?=$place['place_id']?>">
+      <img src="../images/test.jpg" />
+      <p> <?=$place['place_price_per_day']?> </p>
+      <p> <?=$place['place_title']?> </p>
+      <p> <?=$place['place_location']?> </p>
+    </a>
   </article>
 <?php }
 ?>
