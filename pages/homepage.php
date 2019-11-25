3ec6f7d16b3b33bdc0b70d633986985bef49140c
João Rafael Varela <revuj112@gmail.com>
2019-11-25 13:46:08 +0000

    Responsive Places List

--- pages/homepage.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('../templates/tpl_common.php');
-    include_once('../templates/tpl_main.php');
-    draw_header();
-    draw_main_page();
-    draw_footer();
-?>
--- pages/hostplace.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('../templates/tpl_common.php');
-    include_once('../templates/tpl_places.php');
-    draw_header();
-    draw_input_place(); 
-    draw_footer();
-?>
